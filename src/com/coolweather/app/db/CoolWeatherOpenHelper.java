670f2a9eddcda268273fecc05f9ffe19ddaa6b9e
superjiajia <527014965@qq.com>
2015-08-07 16:40:17 +0800

    完成遍历省市县三级列表的功能。

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -11,28 +11,28 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	/**
 	 * Province表建表语句
 	 */
-	public static final String CREATE_PROVINCE = "create table province(" +
-			"id integer primary key autoincrement," +
-			"province_name text," +
-			"province_code text)";
+	public static final String CREATE_PROVINCE = "create table Province(" 
+	 +"id integer primary key autoincrement," 
+	 +"province_name text," 
+	 +"province_code text)";
 	
 	/**
 	 * City表建表语句
 	 */
-	public static final String CREATE_CITY = "create table city(" +
-			"id integer primary key antoincrement," +
-			"city_name text, " +
-			"city_code text," +
-			" province_id integer)";
+	public static final String CREATE_CITY = "create table City(" 
+	 +"id integer primary key autoincrement," 
+	 +"city_name text, "
+	 +"city_code text," 
+	 +" province_id integer)";
 	
 	/**
 	 * Country表建表语句
 	 */
-	public static final String CREATE_COUNTRY = "create table country(" +
-			"id integer primary key antoincrement," +
-			" country_name text, " +
-			"country_code text," +
-			" city_id integer)";
+	public static final String CREATE_COUNTRY = "create table Country(" 
+	 +"id integer primary key autoincrement," 
+	 +" country_name text, " 
+	 +"country_code text,"
+	 +" city_id integer)";
 
 	
 	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version){
